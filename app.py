8bd894956988b125d97e562ee90504f6c16ec788
Precious-OS <precious.osuchukwu@gmail.com>
2025-02-17 21:28:03 -0500

    updated app.py port

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -30,4 +31,5 @@ def predict():
     return result
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
